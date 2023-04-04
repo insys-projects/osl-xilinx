4eb951f625f126bb9aa8c9d08702805c7e7cdd2d
Karakozov Vladimir <karakozov@gmail.com>
2023-04-04 12:58:29 +0300

    Update boot and ramdisk scripts

--- bin/create_ramdisk.sh
+++ bin/create_ramdisk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dd if=/dev/zero of=ramdisk bs=1M count=128
+sudo dd if=/dev/zero of=ramdisk bs=1M count=96
 
 sudo mkfs.ext4 ramdisk
 
@@ -12,7 +12,7 @@ sudo tar -xvpf rootfs.tar -C /tmp/ramdisk
 
 sudo umount /tmp/ramdisk
 
-gzip -9 ramdisk
+gzip -f -9 ramdisk
 
 mkimage -f fit-kernel-full.its image.ub.ramfs
 
